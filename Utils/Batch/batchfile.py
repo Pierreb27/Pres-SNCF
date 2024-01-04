3fd783f8c03c3c1a77a5a0d3e77640cf080b1252
Pierreb27 <pierrebalan72@gmail.com>
2024-01-04 15:12:04 +0100

    Init

--- Utils/Batch/batchfile.py
+++ Utils/Batch/batchfile.py
@@ -1,20 +1,22 @@
 import boto3
+import csv
+import io
 
-session = boto3.session.Session(region_name= 'eu-west-3')
-s3 = session.resource('s3')
+s3 = boto3.resource('s3')
 bucket = s3.Bucket('s3triggerdemo-pb44')
 
 commands = 0
 price = 0
 for obj in bucket.objects.all():
     key = obj.key
-    if key.split(".")[key.split(".").length] == "csv"
-        response = s3Client.get_object(Bucket=bucket, Key=key)
-    
-        data = response['Body'].read().decode()
+    if key.split(".")[-1] == "csv" :
+        commands += 1
+        data = obj.get()['Body'].read().decode()
         reader = csv.reader(io.StringIO(data), delimiter=';')
         next(reader)
         for row in reader:
             price += int(row[1])*int(row[2])
-
-print(str.format("Total commands : {} / Average price : {}", str(commands), str(price/commands)))
+        print(str.format("Command {} treated", key))
+    else :
+        print(str.format("File {} not treated (not a command)", key))
+print(str.format("Number of command(s) : {} / Average command price : {} / Sales revenue : {}", str(commands), str(price/commands), str(price)))
